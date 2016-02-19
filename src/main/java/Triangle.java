4b7a633162aa01c116a915a8e13998e6ac1ee1c6
Ryan Lau <ryanlau92@gmail.com>
2016-02-19 01:39:00 -0600

    Added line programs

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -4,6 +4,9 @@
 public class Triangle {
 
     public static void main(String[] args) {
-        
+        TriangleCreator tC = new TriangleCreator();
+//        tC.easiestExercise();
+//        tC.horizontalLine(8);
+        tC.verticalLine(3);
     }
 }
---
+++ src/main/java/TriangleCreator.java
@@ -0,0 +1,26 @@
+/**
+ * Created by ryanlau on 2/19/16.
+ */
+public class TriangleCreator {
+
+    public void easiestExercise(){
+        System.out.println("*");
+    }
+
+    public void horizontalLine(int length){
+        for (int i = 0; i < length; i++){
+            System.out.print("*");
+        }
+    }
+
+    public void verticalLine(int height){
+        for (int i = 0; i < height; i++){
+            System.out.println("*");
+        }
+    }
+
+    public void rightTriangle(int base){
+        
+    }
+
+}
